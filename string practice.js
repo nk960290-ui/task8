a459991d639fffbfd275e66f4875b5e7e3d0a0a8
nk960290-ui <nk960290@gmail.com>
2026-07-04 15:14:42 +0530

    Add files via upload

---
+++ string practice.js
@@ -0,0 +1,15 @@
+let company = " Stackly IT ";
+
+console.log("Original String:", company);
+
+console.log("Uppercase:", company.toUpperCase());
+
+console.log("Lowercase:", company.toLowerCase());
+
+console.log("Remove Spaces:", company.trim());
+
+console.log("Replace:", company.replace("IT", "Software"));
+
+console.log("Contains 'Stackly':", company.includes("Stackly"));
+
+console.log("Split:", company.trim().split(" "));
